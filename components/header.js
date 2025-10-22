52f821036ca4021d754a52e65c44f07249045f71
Dipu Kumar <dipukumarpojhi@gmail.com>
2025-10-22 12:57:09 +0530

    Links for Navigation

--- components/header.js
+++ components/header.js
@@ -1,11 +1,15 @@
+import Counter from "@/components/counter";
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header className="p-20 border border-blue-900">
         <ul className="flex space-x-4">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/about/projects">Projects</a></li>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/about/projects">Projects</Link></li>
         </ul>
+        <Counter />
     </header>
   )
 }
