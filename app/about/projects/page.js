cfdb13f3089d42df32d42d77ae85a33a14343aa7
Dipu Kumar <dipukumarpojhi@gmail.com>
2025-10-25 11:23:18 +0530

    fetching data with fetch inside a server component

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,6 +1,21 @@
-const ProjectsPage = () => {
+const ProjectsPage = async () => {
+  const response = await fetch('http://localhost:3001/repos')
+  const repos = await response.json()
+
   return (
-    <div>Projects! This won&apos;t be visible.</div>
+    <div className="p-20">
+      <h1 className="mb-8 text-xl">Projects</h1>
+
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.id} className="mb-4">
+            <div>{repo.title}</div>
+            <div>{repo.description}</div>
+            <div>{repo.stargazers_count}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
